a1c8b696cd5419f5065495c5a05d069bd02edb8a
unknown <thi.nguyen2307@gmail.com>
2020-04-23 16:20:43 -0700

    Add data structure--stack

---
+++ stack.py
@@ -0,0 +1,47 @@
+class Stack:
+    def __init__(self):
+        self.items = []
+
+    def push(self,item):
+        """Add item to end of list and return nothing. Run time for this method is O(1) or constant time"""
+        return self.items.append(item)
+    
+    def pop(self):
+        if self.items:
+            return self.items.pop()
+        return None
+
+    def size(self):
+        return len(self.items)
+
+    def is_empty(self):
+        return self.items == []
+
+# check symbol string balance
+def match_symbols(symbols):
+    symbol_pairs = {
+        "{":"}",
+        "[":"]",
+        "(":")"
+    }
+    openers = symbol_pairs.keys()
+    symbol_stack = Stack()
+
+    i = 0
+    while i < len(symbols):
+        symbol = symbols[i]
+        
+        if symbol in openers:
+            symbol_stack.push(symbol)
+        else: # The symbol is a closer
+            if symbol_stack.is_empty():
+                return False
+            else:
+                top_item = symbol_stack.pop()
+                if symbol != symbol_pairs[top_item]:
+                    return False
+        i += 1
+    if symbol_stack.is_empty():
+        return True
+
+print(match_symbols("{[()]}())"))
